42236fb717afa373dc7a148fa10af70ba2a6a4fb
q <diamond264@naver.com>
2017-07-03 20:00:27 +0900

    사진 확대 추가

--- app/src/main/java/com/example/q/a496project/Tab3Medias.java
+++ app/src/main/java/com/example/q/a496project/Tab3Medias.java
@@ -1,10 +1,14 @@
 package com.example.q.a496project;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.MediaController;
+import android.widget.VideoView;
+import android.widget.BaseAdapter;
 
 /**
  * Created by q on 2017-06-30.
@@ -15,7 +19,15 @@ public class Tab3Medias extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.tab3_media, container, false);
-        return rootView;
+        View view = inflater.inflate(R.layout.tab3_media, container, false);
+
+        VideoView videoView = (VideoView) view.findViewById(R.id.videoView);
+        Uri uri = Uri.parse("rtsp://v6.cache4.c.youtu.be/tWRT6LrZGWQ");
+        videoView.setVideoURI(uri);
+        final MediaController mediaController = new MediaController(getActivity());
+        videoView.setMediaController(mediaController);
+        videoView.start();
+        ((ViewGroup)videoView.getParent()).removeView(videoView);
+        return videoView;
     }
 }
